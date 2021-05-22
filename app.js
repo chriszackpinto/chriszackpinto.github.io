fb1ff322339953b7cc365499499f45b3fd81590d
zackendev <chrispinto2197@gmail.com>
2021-05-23 01:09:02 +0530

    Switched to efficient scrolling animation

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mainTitle = document.querySelector(".main-title");
 const logo = document.querySelector(".header-nav-logo");
 const link = document.querySelector(".header-nav-list");
 
+const projects = document.querySelector(".projects");
+const projectList = document.querySelector(".projects-list");
+
 const tl = new TimelineMax();
 
 tl.fromTo(mainTitle, 1, { height: "0%" }, { height: "100%", ease: "none" })
@@ -11,36 +14,37 @@ tl.fromTo(mainTitle, 1, { height: "0%" }, { height: "100%", ease: "none" })
     mainTitle,
     1.2,
     { width: "1920px" },
-    { width: "1200px", ease: "bounce.out" }, "-=0.2"
+    { width: "1200px", ease: "bounce.out" },
+    "-=0.2"
   )
   .fromTo(logo, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
   .fromTo(link, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5");
 
-// Hide background on scroll
-window.onscroll = function () {
-  scrollFunction();
+const bgColor = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) mainTitle.style.backgroundColor = "#16ade3";
+  else mainTitle.style.backgroundColor = "#1e1e20";
 };
 
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 300 ||
-    document.documentElement.scrollTop > 300
-  ) {
-    mainTitle.style.backgroundColor = "#1e1e20";
-  } else {
-    mainTitle.style.backgroundColor = "#16ade3";
-  }
-}
+const splashObserver = new IntersectionObserver(bgColor, {
+  root: null,
+  threshold: 0.8,
+});
 
-function scrollApp() {
-  let projectItem1 = document.querySelector(`.project1`);
-  let projectItem2 = document.querySelector(`.project2`);
-  let itemPosition = projectItem1.getBoundingClientRect().top;
-  let screenPosition = window.innerHeight / 1.2;
+splashObserver.observe(projects);
 
-  if (itemPosition < screenPosition) {
-    projectItem1.classList.add("appear");
-    projectItem2.classList.add("appear");
-  }
-}
-window.addEventListener(`scroll`, scrollApp);
+projectList.classList.add("item-hidden");
+
+const revealItem = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("item-hidden");
+  observer.unobserve(entry.target);
+};
+
+const itemObserver = new IntersectionObserver(revealItem, {
+  root: null,
+  threshold: 0,
+});
+
+itemObserver.observe(projectList);
